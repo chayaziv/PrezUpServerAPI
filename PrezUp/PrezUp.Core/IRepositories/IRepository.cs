44af6c20d355f5895ec4689b5a08285a5c23c4af
chayaziv <chy15658@gmail.com>
2025-03-07 01:04:11 +0200

    Add IRepository and IPresentationService interfaces for data access and service layer

---
+++ PrezUp/PrezUp.Core/IRepositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrezUp.Core.IRepositories
+{
+    interface IRepository<T>
+    {
+        public List<T> GetList();
+
+        public T GetById(int id);
+
+        public T Add(T val);
+
+        public T Update(T val);
+
+        public void Delete(T id);
+    }
+}
---
+++ PrezUp/PrezUp.Core/IServices/IPresentationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrezUp.Core.Entity;
+
+namespace PrezUp.Core.IServices
+{
+    interface IPresentationService
+    {
+        public Task<List<Presentation>> getallAsync();
+
+        public Task<Presentation> getByIdAsync(int id);
+
+        public Task<Presentation> addAsync(Presentation agreement);
+
+        public Task<Presentation> updateAsync(int id, Presentation agreement);
+
+        public Task<bool> deleteAsync(int id);
+    }
+}
