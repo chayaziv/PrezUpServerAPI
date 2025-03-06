47603ff92ca965ae755884d8afdecf1b6bb230a2
chayaziv <chy15658@gmail.com>
2025-03-07 01:04:24 +0200

    Add Presentation controller and related repository structure

---
+++ PrezUp/PrezUp.Core/IRepositories/IRepositoryManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrezUp.Core.Entity;
+
+namespace PrezUp.Core.IRepositories
+{
+    interface IRepositoryManager
+    {
+        public IRepository <Presentation> Presentations { get; }
+
+        Task<int> SaveAsync();
+
+    }
+}
